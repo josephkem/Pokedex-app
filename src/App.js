9148bb98656d78580dcff96854c61b34f61427d3
josephkem <josephkemoy@gmail.com>
2021-10-14 19:43:24 -0400

    Added components to the navbar along with some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Pokedex from "./pages/Pokedex";
+import Navigator from "./components/Navigator";
 
 function App() {
   return (
     <Router>
+      <Navigator />
       <Route exact path="/" component={Pokedex} />
     </Router>
   );
--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  AppBar: {
+    backgroundColor: "black",
+  },
+
+  link: {
+    textDecoration: "none",
+  },
+  title: {
+    cursor: "pointer",
+    color: "red",
+  },
+}));
 
 const Navigator = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <AppBar position="fixed" className={classes.AppBar}>
+      <Toolbar>
+        <Link to="/" className={classes.link}>
+          <Typography className={classes.title} variant="h6">
+            Pokedex
+          </Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 };
 
