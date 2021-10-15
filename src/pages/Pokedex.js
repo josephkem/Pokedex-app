81d2b89f77377268927d4a24325796bb65ca5207
josephkem <josephkemoy@gmail.com>
2021-10-14 20:43:46 -0400

    Added config file to store variables and created object from the http repsonse to store the data

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box } from "@material-ui/core";
+import axios from "axios";
+import { POKEMON_API_URL, IMAGE_API_URL } from "../config/index";
 
 function Pokedex() {
+  useEffect(() => {
+    axios.get(POKEMON_API_URL + "?limit=8").then((res) => {
+      console.log(res.data.results);
+      if (res.status >= 200 && res.status < 300) {
+        const { results } = res.data;
+
+        let newPokemonData = [];
+
+        results.forEach((pokemon, index) => {
+          index++;
+          let pokemonObject = {
+            id: index,
+            url: IMAGE_API_URL + index + ".png",
+            name: pokemon.name,
+          };
+          console.log("pokemonObject", pokemonObject);
+          console.log("pokemon", pokemon);
+        });
+      }
+    });
+  }, []);
   return (
-    <div>
+    <Box>
       <h1>Pokemon</h1>
-    </div>
+    </Box>
   );
 }
 
