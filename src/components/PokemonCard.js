ed3454f197d632f8f39fe8d501e11515c2722b67
josephkem <josephkemoy@gmail.com>
2021-10-14 21:34:09 -0400

    Added styling to the card components

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -6,14 +6,35 @@ import {
   Grid,
   Typography,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  cardMedia: {
+    margin: "auto",
+    width: 130,
+    height: 130,
+  },
+  card: {
+    cursor: "pointer",
+    backgroundColor: "black",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "rgb(90,90,90)",
+    },
+  },
+  cardContent: {
+    textAlign: "center",
+  },
+}));
 
 const PokemonCard = ({ pokemon, image }) => {
+  const classes = useStyles();
   const { id, name } = pokemon;
   return (
     <Grid item xs={12} sm={2}>
-      <Card>
-        <CardMedia image={image} />
-        <CardContent>
+      <Card className={classes.card}>
+        <CardMedia image={image} className={classes.cardMedia} />
+        <CardContent className={classes.cardContent}>
           <Typography>{name}</Typography>
         </CardContent>
       </Card>
