83c892b2a215ea7dddbef8f1963c0ebd7a0c9b6a
josephkem <josephkemoy@gmail.com>
2021-10-14 21:55:30 -0400

    Created page for pokemon details and added a route to the main app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Pokedex from "./pages/Pokedex";
 import Navigator from "./components/Navigator";
+import PokemonDetails from "./pages/PokemonDetails";
 
 function App() {
   return (
     <Router>
       <Navigator />
       <Route exact path="/" component={Pokedex} />
+      <Route exact path="/pokemon/:id" component={PokemonDetails} />
     </Router>
   );
 }
---
+++ src/pages/PokemonDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PokemonDetails = () => {
+  return (
+    <div>
+      <h1>Details</h1>
+    </div>
+  );
+};
+
+export default PokemonDetails;
